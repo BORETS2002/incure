b7db4b37c2ce6d9671aa77215c8c18f195ece41e
BORETS2002 <BORETS2002@example.com>
2022-08-13 20:50:26 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var elSiteheaderToggleBtn = document.querySelector(".site-header__menu-btn");
+var elHeader = document.querySelector(".site-header");
+var elsiteNav = document.querySelector(".sitenav");
+
+elSiteheaderToggleBtn.addEventListener("click", function () {
+  elHeader.classList.toggle("site-header__open");
+});
+
+elSiteheaderToggleBtn.addEventListener("click", function () {
+  elHeader.classList.toggle("site-header__fixsed");
+});
